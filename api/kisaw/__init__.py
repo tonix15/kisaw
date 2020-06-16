2037accc39bde21a82827f6d54c89154be21b920
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-16 22:17:54 +0800

    Add Flask Migration functionality

--- api/kisaw/__init__.py
+++ api/kisaw/__init__.py
@@ -1,6 +1,10 @@
+import time
+import click
+
 from flask import Flask
 
-import time
+from .db import db, migrate
+from .db.models import User
 
 from .blueprints.articles import articles_bp
 
@@ -13,6 +17,19 @@ def create_app(config=None):
     else:
         app.config.from_pyfile(config)
 
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(articles_bp, url_prefix='/api')
+
+    @click.command('create-user')
+    @click.option('--username', required=True)
+    @click.option('--password', required=True)
+    @click.option('--email', required=True)
+    def create_user(email, username, password):
+        user = User(email=email, username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        click.echo('Created user {}'.format(username))
     
     return app
