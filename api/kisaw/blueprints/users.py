ba4895d5511e7bbfc25a418f233c6114a24857c8
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-20 05:24:06 +0800

    Add CRUD and register to user's blueprint

---
+++ api/kisaw/blueprints/users.py
@@ -0,0 +1,74 @@
+import json
+
+from pprint import pprint
+
+from flask import Blueprint, request, make_response
+
+from ..db import db
+from ..db.models import User
+from ..db.schemas import UserSchema
+
+users_bp = Blueprint('users_bp', __name__)
+
+
+@users_bp.route('/users', methods=('GET',))
+def index():
+    users = User.query.all()
+
+    if not users:
+        return make_response({'msg': 'No users yet.'}), 204
+    
+    user_schema = UserSchema(many=True, exclude=('password',))
+    serialized_result = user_schema.dump(users)
+    
+    return make_response({'users': serialized_result}), 200
+
+
+@users_bp.route('/user/<id>', methods=('GET', 'POST', 'PUT', 'DELETE'))
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        return make_response({'msg': 'User not found.'}), 400
+    
+    if request.method == 'GET':
+        user_schema = UserSchema(exclude=('password',))
+        serialized_result = user_schema.dump(user)
+        return make_response({'user': serialized_result}), 200
+        
+    elif request.method == 'PUT':
+        request_data = request.get_json()
+
+        keys = request_data.keys()
+
+        if 'email' in keys:
+            user.email = request_data['email']
+
+        if 'username' in keys:
+            user.username = request_data['username']
+
+        if 'password' in keys:
+            user.password = request_data['password']
+
+        if 'first_name' in keys:
+            user.first_name = request_data['first_name']
+
+        if 'last_name' in keys:
+            user.last_name = request_data['last_name']
+
+        db.session.commit()
+        return make_response({'msg': '{} updated.'.format(user.username)}), 202
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        return make_response({'msg': '{} deleted.'.format(user.username)}), 202
+
+
+@users_bp.route('/register', methods=('POST',))
+def register():
+    request_data = request.get_json()
+    
+    user = User(email=request_data['email'], username=request_data['username'], password=request_data['password'])
+    db.session.add(user)
+    db.session.commit()
+
+    return {'msg': '{} added.'.format(user.username)}, 201
