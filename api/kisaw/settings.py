ca7aed452739445b262a67ab9dc68318bf164ce2
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-21 03:43:40 +0800

    Added the JWT Token secret

--- api/kisaw/settings.py
+++ api/kisaw/settings.py
@@ -5,6 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = environ.get('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'kisaw.db')
+JWT_SECRET = environ.get('JWT_SECRET')
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'kisaw.db')
 SQLALCHEMY_ECHO=True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
