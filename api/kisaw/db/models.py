8d65c45d37d898a5d3f6b85f7ce79e7e0d52e098
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-26 00:14:18 +0800

    Clean up and migrations and setup Realtionships

--- api/kisaw/db/models.py
+++ api/kisaw/db/models.py
@@ -12,6 +12,11 @@ class BaseModel(db.Model):
                            onupdate=datetime.utcnow)
 
 
+class Role(BaseModel):
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    users = db.relationship('User', backref='role', lazy=True)    
+    
 class User(BaseModel):
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
@@ -19,6 +24,8 @@ class User(BaseModel):
     first_name = db.Column(db.String, nullable=True)
     last_name = db.Column(db.String, nullable=True)
     photo = db.Column(db.String, nullable=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)    
+    articles = db.relationship('Article', backref='user', lazy=True)
 
     @validates('password')
     def hash_password(self, key, password):
@@ -32,7 +39,48 @@ class Article(BaseModel):
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.Text)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('users', lazy=True))
 
+    # This will be used to show or hide an article
+    published = db.Column(db.Boolean)
+    metadatas = db.relationship('ArticleMeta', backref='article', lazy=True)
+    comments = db.relationship('Comment', backref='article', lazy=True)
+    
     def __repr__(self):
         return '<Article %r>' % self.title
+
+class ArticleMeta(BaseModel):
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+    meta_title = db.Column(db.String, nullable=False)
+    meta_banner = db.Column(db.String)
+
+
+class Comment(BaseModel):
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+    parent_id = db.Column(db.Integer, nullable=True, default=0)
+    body = db.Column(db.Text)
+
+    # This will be used to show or hide a comment
+    published = db.Column(db.Boolean, default=True)
+
+
+article_category = db.Table('article_category',
+    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+class Category(BaseModel):
+    title = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return '<Category %r>' % self,title
+
+
+article_tag = db.Table('article_tag',
+    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
+
+class Tag(BaseModel):
+    name  = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
