67365a6d0d9b1006b3279e5da5bb1eddef0207cd
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-26 00:17:47 +0800

    Add Roles Blueprint

--- api/kisaw/__init__.py
+++ api/kisaw/__init__.py
@@ -8,6 +8,7 @@ from .db.models import User
 
 from .blueprints.auth import auth_bp
 from .blueprints.articles import articles_bp
+from .blueprints.roles import roles_bp
 from .blueprints.users import users_bp
 
 
@@ -24,6 +25,7 @@ def create_app(config=None):
 
     app.register_blueprint(auth_bp, url_prefix='/api/v1')
     app.register_blueprint(articles_bp, url_prefix='/api/v1')
+    app.register_blueprint(roles_bp, url_prefix='/api/v1')
     app.register_blueprint(users_bp, url_prefix='/api/v1')
 
     @click.command('create-user')
