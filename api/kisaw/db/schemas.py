a8e6b99248e5c777a71c9796e1446ad9765c2bd7
Anthony Gerard Mondoy <tonix15@gmail.com>
2020-06-26 00:18:38 +0800

    Add Role marshmallow schema

--- api/kisaw/db/schemas.py
+++ api/kisaw/db/schemas.py
@@ -1,16 +1,26 @@
 from marshmallow import Schema, fields
 
+class RoleSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    description = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+
 class UserSchema(Schema):
+    id = fields.Int()
     email = fields.Email()
     username = fields.Str()
     password = fields.Str()
     first_name = fields.Str()
     last_name = fields.Str()
     photo = fields.Str()
+    role_id = fields.Int()
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
 
 class ArticleSchema(Schema):
+    id = fields.Int()
     title = fields.Str()
     body = fields.Str()
     author_id = fields.Int()
